a792338e8b5bff707cc6ce132c28b6d8d1b7b71f
Drajat Hasan <drajathasan20@gmail.com>
2021-12-06 21:52:57 +0700

    Add : Tui dan Vanilla Picker

--- view/colorscheme.php
+++ view/colorscheme.php
@@ -1,1 +1,13 @@
-Hai
+<?php
+/**
+ * @author Drajat Hasan
+ * @email drajathasan20@gmail.com
+ * @create date 2021-12-06 21:26:47
+ * @modify date 2021-12-06 21:26:47
+ * @license GPLv3
+ * @desc [description]
+ */
+
+
+
+defined('INDEX_AUTH') or die('No direct access!');
